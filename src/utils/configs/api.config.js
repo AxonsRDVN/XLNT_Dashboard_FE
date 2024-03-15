bfcddccedbdfc3cc73afed4160565e1a87fbc4b5
controlboy1411 <khanhhungithn@gmail.com>
2024-03-15 14:02:45 +0700

    first commit

---
+++ src/utils/configs/api.config.js
@@ -0,0 +1,3 @@
+// export const HOST_URL_API = 'https://52.221.213.126:443';
+export const HOST_URL_API = 'https://xlnt-dashboard-be.onrender.com';
+// export const HOST_URL_API = 'http://localhost:9031';
